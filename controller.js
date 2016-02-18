8d433ef4af587f5f56f359074c649dec753c256d
Fernando Lafuente saiz <fernandolafuentesaiz@gmail.com>
2016-02-18 17:33:30 +0100

    group list using koa-grid.

--- controller.js
+++ controller.js
@@ -3,17 +3,30 @@ angular
 
 loadFunction.$inject = ['$scope', 'structureService', '$location'];
 
-function loadFunction($scope, structureService, $location){
+function loadFunction($scope, structureService, $location) {
   //Register upper level modules
-  structureService.registerModule($location,$scope,"grouplist");
-  console.log($scope.grouplist.modulescope);
+  structureService.registerModule($location, $scope, "grouplist");
+
   var list = [];
-  angular.forEach($scope.grouplist.modulescope.sections, function(value, key){
-    if(structureService.get().modules[value.replace(/#/g, '')]){
+
+  angular.forEach($scope.grouplist.modulescope.sections, function(value, key) {
+    if (structureService.get().modules[value.replace(/#/g, '')]) {
+
       var name = structureService.get().modules[value.replace(/#/g, '')].name;
-      list.push( { name:name, url:value.replace(/#/g, '') } );
+      var icon = structureService.get().modules[value.replace(/#/g, '')].icon;
+      var bg = "";
+      if($scope.grouplist.modulescope.bgs[key]){
+        bg = $scope.grouplist.modulescope.bgs[key];
+      }
+
+      list.push({
+        name: name,
+        bg: bg,
+        url: value.replace(/#/g, ''),
+        icon: icon
+      });
     }
 
   });
-  $scope.grouplist.modulescope.newsections=list;
+  $scope.grouplist.modulescope.newsections = list;
 }
